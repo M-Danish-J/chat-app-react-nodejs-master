3e5233b09d14a92f79e8eeee22ae3333c3400373
i-usman-56 <ranausman417335@gmail.com>
2023-11-13 22:12:17 +0500

    Update Register.jsx

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -117,7 +117,7 @@ export default function Register() {
           process.env.REACT_APP_LOCALHOST_KEY,
           JSON.stringify(data.user)
         );
-        navigate("/");
+        navigate("/login");
       }
     }
   };
